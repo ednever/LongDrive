25aaad98055fe505ccc12be441dbdf6fa89b2df8
opilane <opilane@HP-CZC2349HTB>
2023-09-21 12:07:31 +0300

    + select

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [selectedOption1, setSelectedOption1] = useState('');
-  const [selectedOption2, setSelectedOption2] = useState('');
+  //const [selectedOption1, setSelectedOption1] = useState('');
+  //const [selectedOption2, setSelectedOption2] = useState('');
+  const [soiduAutod, setSoiduautod] = useState([]);
+  const [veoAutod, setVeoautod] = useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7101/Soiduauto") //показ всех продуктов
+      .then(res => res.json())
+      .then(json => setSoiduautod(json));
+    fetch("https://localhost:7101/Veoauto")
+      .then(res => res.json())
+      .then(json => setVeoautod(json));
+  }, []);
 
   return (
     <div className="App">
       <div className="App-body">
         <div className="App-container">
-          <header className="App-header">
-            <h1>Выберите</h1>
-          </header>
-          <select
-            value={selectedOption1}
-            onChange={(e) => setSelectedOption1(e.target.value)}
-          >
-            <option value="">Выбор 1</option>
-            <option value="Опция 1">Опция 1</option>
-            <option value="Опция 2">Опция 2</option>
+
+          <header className="App-header"><h1>Long Drive</h1></header>
+          <header className="App-header"><h3>Выберите</h3></header>
+          <div>
+          <select>
+            <option value="Машины">Машины</option>
+            {soiduAutod.map((auto) => (<option value={auto.mark}> {auto.mark} </option>))}
           </select>
-          <select
-            value={selectedOption2}
-            onChange={(e) => setSelectedOption2(e.target.value)}
-          >
-            <option value="">Выбор 2</option>
-            <option value="Опция A">Опция A</option>
-            <option value="Опция B">Опция B</option>
+
+          <select>
+            <option value="Грузовики">Грузовики</option>
+            {veoAutod.map((auto) => (<option value={auto.mark}> {auto.mark} </option>))}
           </select>
+
+          </div>
           <button>Добавить</button>
+          
         </div>
       </div>
     </div>
@@ -36,3 +44,4 @@ function App() {
 }
 
 export default App;
+//value={selectedOption1} onChange={(e) => setSelectedOption1(e.target.value)}
