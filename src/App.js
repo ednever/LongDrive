5f1486542e73e1b45214b5e6eff3ae6ee08f8e78
Denis Saiko <93324253+Mvsterpiece@users.noreply.github.com>
2023-09-21 09:16:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedOption1, setSelectedOption1] = useState('');
+  const [selectedOption2, setSelectedOption2] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+        <div className="App-container">
+          <header className="App-header">
+            <h1>Выберите</h1>
+          </header>
+          <select
+            value={selectedOption1}
+            onChange={(e) => setSelectedOption1(e.target.value)}
+          >
+            <option value="">Выбор 1</option>
+            <option value="Опция 1">Опция 1</option>
+            <option value="Опция 2">Опция 2</option>
+          </select>
+          <select
+            value={selectedOption2}
+            onChange={(e) => setSelectedOption2(e.target.value)}
+          >
+            <option value="">Выбор 2</option>
+            <option value="Опция A">Опция A</option>
+            <option value="Опция B">Опция B</option>
+          </select>
+          <button>Добавить</button>
+        </div>
+      </div>
     </div>
   );
 }
