a784fa466d25279f6de218160b045faf73ff42fd
ednever <edgarneverovskij@gmail.com>
2023-10-20 14:05:32 +0300

    :heart:

--- src/ManguDrive.js
+++ src/ManguDrive.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import './CarAnimation.css';
-import bgg from './images/bgg.png';
+import bgg from './images/bkcc.jpg';
 import w211 from './images/w211.png';
 
 function ManguDrive() {  
@@ -11,19 +11,23 @@ function ManguDrive() {
     const [soiduAuto, setSoiduAuto] = useState(null);
     var piltRef = useRef('');
 
-    const moveLeft = () => {
-      setLeftPosition(leftPosition - 10); 
-    };
-  
-    const moveRight = () => {
-      setLeftPosition(leftPosition + 10);
-    };   
+    //const [carImage, setCarImage] = useState(null);     
 
     useEffect(() => {
       fetch("https://localhost:7101/Soiduauto/" + JSON.stringify(carId))
         .then(res => res.json())
         .then(json => setSoiduAuto(json));
-      w211 = require('./images/w211.png');         
+      w211 = require('./images/w211.png');  
+      
+      /*const loadImage = async () => {
+        const image = new Image();
+        image.src = w211Image;
+        await image.decode();
+        setCarImage(image);
+      };
+  
+      loadImage();*/
+
     }, []);  
 
     if (carHavePicture === 'true' && soiduAuto != null) {
@@ -32,39 +36,50 @@ function ManguDrive() {
       piltRef = w211;
     }   
 
-    
-
-    const objectStyle = {
-      position: 'absolute',
-      bottom: '-5px',
-      left: `${leftPosition}px`,
+    const moveLeft = () => {
+      setLeftPosition(leftPosition - 10); 
     };
   
-    const pedalStyle = {
-      position: 'absolute',
-      top: '0',
-      left: 0,
-      width: '200px',
-      height: '100px',
-      border: '1px solid black',
-      transform: 'translateX(-50%)',
-    };
+    const moveRight = () => {
+      setLeftPosition(leftPosition + 10);
+    }; 
+
+    /*const handleKeyDown = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          moveLeft();
+          break;
+        case "ArrowRight":
+          moveRight();
+          break;
+        default:
+          break;
+      }
+    };*/
+
+    /*useEffect(() => {
+      document.body.style.overflow = "hidden";
+  
+      document.addEventListener("keydown", handleKeyDown);
+      
+      return () => {
+        document.body.style.overflow = "visible";
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [leftPosition]);*/
   
     return (
-      <div>
-        <img src={bgg} style={{
-          width: '100%',
-          height: '100vh',
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          zIndex: -1,
-        }} />       
-        <div style={objectStyle}><img src={piltRef} alt='pilt' /></div>
-        <button onClick={moveLeft} className="pedal">Влево</button>
-        <button onClick={moveRight} className="pedal">Вправо</button>
+      <div className="container">
+        <img src={bgg} className="background" alt="Background" />
+        <div className="car-object" style={{ left: `${leftPosition}px` }}>
+          {carImage && <img src={carImage.src} alt="Car" />}
+        </div>
+        <button onClick={moveLeft} className="pedal left-pedal">Тормоз</button>
+        <button onClick={moveRight} className="pedal right-pedal">Газ</button>
       </div>
     );
 
 }
 export default ManguDrive;
+
+//Изменить картинку на piltRef
